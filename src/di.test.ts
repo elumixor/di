7f8596ee5e215a28919ea1d91470b45866639fc9
Vladyslav Yazykov <vladogim97@gmail.com>
2025-09-07 02:25:01 +0200

    refactor: Refactoring and updates to biome, etc.

---
+++ src/di.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "bun:test";
+
+describe("Dependency Injection", () => {
+  it("should resolve dependencies", () => {
+    expect(true).toBe(true);
+  });
+});
--- src/di.ts
+++ src/di.ts
@@ -1,15 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+// biome-ignore-all lint/suspicious/noExplicitAny: It makes sense here
 type AnyConstructor<T extends object = object, TArgs extends any[] = any[]> =
-    | AbstractConstructor<T, TArgs>
-    | Constructor<T, TArgs>;
+  | AbstractConstructor<T, TArgs>
+  | Constructor<T, TArgs>;
 type Constructor<T extends object = object, TArgs extends any[] = any[]> = new (...params: TArgs) => T;
 type AbstractConstructor<T extends object = object, TArgs extends any[] = any[]> = abstract new (...params: TArgs) => T;
 
 const target = globalThis as typeof globalThis & { __di_injectable_map__?: Map<unknown, object> };
 if (!Reflect.has(target, "__di_injectable_map__"))
-    Reflect.set(target, "__di_injectable_map__", new Map<unknown, object>());
+  Reflect.set(target, "__di_injectable_map__", new Map<unknown, object>());
 
-const serviceMap = target.__di_injectable_map__!;
+const serviceMap = target.__di_injectable_map__ as Map<unknown, object>;
 
 /**
  * Obtains the instance of the required service.
@@ -23,10 +23,10 @@ const serviceMap = target.__di_injectable_map__!;
 function inject<T extends object>(service: AnyConstructor<T>, options?: { optional?: false }): T;
 function inject<T extends object>(service: AnyConstructor<T>, options: { optional: true }): T | undefined;
 function inject<T extends object>(service: AnyConstructor<T>, options?: { optional?: boolean }) {
-    const instance = serviceMap.get(service);
-    if (instance) return instance as T;
-    if (options?.optional) return undefined;
-    throw new Error(`No provider for ${service.name}`);
+  const instance = serviceMap.get(service);
+  if (instance) return instance as T;
+  if (options?.optional) return undefined;
+  throw new Error(`No provider for ${service.name}`);
 }
 
 /**
@@ -35,9 +35,9 @@ function inject<T extends object>(service: AnyConstructor<T>, options?: { option
  * @param instance The instance of the service to be provided.
  */
 function provide<T extends object>(service: AnyConstructor<T>, instance: T) {
-    if (serviceMap.has(service)) throw new Error(`Provider ${service.name} is already specified`);
+  if (serviceMap.has(service)) throw new Error(`Provider ${service.name} is already specified`);
 
-    serviceMap.set(service, instance);
+  serviceMap.set(service, instance);
 }
 
 /**
@@ -45,32 +45,31 @@ function provide<T extends object>(service: AnyConstructor<T>, instance: T) {
  * @param service The class of the service.
  */
 function isProvided(service: AnyConstructor) {
-    return serviceMap.has(service);
+  return serviceMap.has(service);
 }
 
 function injectable<TBase extends AnyConstructor>(Base: TBase): TBase {
-    if (isProvided(Reflect.getPrototypeOf(Base) as AnyConstructor))
-        throw new Error("Trying to provide a different child for the same provided parent");
+  if (isProvided(Reflect.getPrototypeOf(Base) as AnyConstructor))
+    throw new Error("Trying to provide a different child for the same provided parent");
 
-    class Wrapper extends Base {
-        constructor(...args: ConstructorParameters<TBase>) {
-            if (isProvided(Wrapper))
-                throw new Error("Trying to provide a different child for the same provided parent");
+  class Wrapper extends Base {
+    constructor(...args: ConstructorParameters<TBase>) {
+      if (isProvided(Wrapper)) throw new Error("Trying to provide a different child for the same provided parent");
 
-            // Base may either be a constructor or an abstract constructor
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-            super(...args);
-            provide(Wrapper, this);
-        }
+      // Base may either be a constructor or an abstract constructor
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      super(...args);
+      provide(Wrapper, this);
     }
+  }
 
-    Reflect.defineProperty(Wrapper, "name", { value: Base.name });
+  Reflect.defineProperty(Wrapper, "name", { value: Base.name });
 
-    return Wrapper;
+  return Wrapper;
 }
 
 function uninject(Base: AnyConstructor) {
-    serviceMap.delete(Base);
+  serviceMap.delete(Base);
 }
 
 export const di = { inject, provide, injectable, uninject };
